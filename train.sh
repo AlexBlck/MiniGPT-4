88c8082605b716dcfcc50173f8a2793302edcd0a
Black, Alex (PG/R - Comp Sci & Elec Eng) <alex.black@surrey.ac.uk>
2024-01-20 10:06:02 +0000

    train sh

---
+++ train.sh
@@ -0,0 +1,8 @@
+cd /mnt/fast/nobackup/users/aj00869/chapter4/minigpt/
+source /opt/conda/bin/activate
+conda init bash
+conda activate /mnt/fast/nobackup/scratch4weeks/aj00869/conda_env/minigpt4
+git checkout $1
+/mnt/fast/nobackup/scratch4weeks/aj00869/conda_env/minigpt4/bin/torchrun --nproc-per-node 1 train.py --cfg-path train_configs/minigptv2_finetune.yaml
+
+
