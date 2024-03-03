4b4e4874d54fc50b57f122d60d8eaba0d0732ccc
Alexander Black <alex.black@surrey.ac.uk>
2024-03-03 23:08:09 +0000

    Change conda env

--- train.sh
+++ train.sh
@@ -1,8 +1,8 @@
 cd /mnt/fast/nobackup/users/aj00869/chapter4/minigpt/
 source /opt/conda/bin/activate
 conda init bash
-conda activate /mnt/fast/nobackup/scratch4weeks/aj00869/conda_env/minigpt4
+conda activate /mnt/fast/nobackup/scratch4weeks/aj00869/conda_env/mgp
 git checkout $1
-/mnt/fast/nobackup/scratch4weeks/aj00869/conda_env/minigpt4/bin/torchrun --nproc-per-node 4 train.py --cfg-path train_configs/minigptv2_finetune.yaml
+/mnt/fast/nobackup/scratch4weeks/aj00869/conda_env/mgp/bin/torchrun --nproc-per-node 4 train.py --cfg-path train_configs/minigptv2_finetune.yaml
 
 
